61e836b74b640c6c2fe3e1198721af8c9163e9f0
Faizy Devsoft <devsoft2110@gmail.com>
2023-01-11 17:31:34 +0500

    worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { ReactTagify } from "react-tagify";
+import Tagify from "@yaireo/tagify";
 
 function App() {
+	const defaultTags = [{ value: "foo", editable: false }, { value: "bar" }];
+
+	const [tags, setTags] = useState(defaultTags);
+
+	const inputElement = document.querySelector("input");
+	new Tagify(inputElement);
+
+	useEffect(() => {}, []);
+
 	return (
 		<div className="App">
-			<ReactTagify colors={"red"} tagClicked={(tag) => alert(tag)}>
-				<p>
-					“You might not think that #programmers are #artists, but programming is an extremely creative #profession. Its
-					logic-based creativity” @JohnRomero
-				</p>
-			</ReactTagify>
+			<input
+				onChange={(e) => {
+					console.log(e);
+				}}
+				value={JSON.stringify(tags.value)}
+			/>
+
+			<button>Add</button>
 		</div>
 	);
 }
